3ea95a55a402d1a4f4ee993d65c277900e5d3478
eacosta1956 <eldon.costa@terra.com.br>
2024-04-06 12:26:15 -0300

    Adicionado o codigo da Tela Listar Produtos

--- src/screens/ListarProdutosScreen.js
+++ src/screens/ListarProdutosScreen.js
@@ -1,10 +1,61 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { db } from '../database/SQLite';
 
-export default function ListarProdutosScreeneen() {
+export default function ListarProdutosScreen() {
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(() => {
+    carregarProdutos();
+  }, []);
+
+  const carregarProdutos = () => {
+    db.transaction((transaction) => {
+      transaction.executeSql(
+        `SELECT nome_produto, estoque_seguranca, estoque_minimo, data_cadastro FROM produtos;`,
+        [],
+        (_, { rows }) => {
+          const produtosList = rows._array; // Convertendo os resultados em um array
+          setProdutos(produtosList);
+        },
+        (_, error) => {
+          console.log('Erro ao listar produtos: ' + error);
+        }
+      );
+    });
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Text>Nome: {item.nome_produto}</Text>
+      <Text>Estoque Segurança: {item.estoque_seguranca}</Text>
+      <Text>Estoque Mínimo: {item.estoque_minimo}</Text>
+      <Text>Data Cadastro: {item.data_cadastro}</Text>
+    </View>
+  );
+
   return (
-    <View>
-      <Text>Listar Produto Screen</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={produtos}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.nome_produto}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 10,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+});
+
